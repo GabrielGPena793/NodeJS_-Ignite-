ece0b22360dcd8f16c64f88382fd62f1b86331af
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-07-30 04:15:21 -0300

    add midleware and utils

---
+++ Api/src/utils/file.ts
@@ -0,0 +1,13 @@
+import fs from "fs"
+
+
+export const deleteFile = async(filename: string) => {
+
+  try{
+    await fs.promises.stat(filename)  // verifica se o arquivo existe
+  }catch {
+    return;
+  }
+
+  await fs.promises.unlink(filename) // remove o arquivo
+}
